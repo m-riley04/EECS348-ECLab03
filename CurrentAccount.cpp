18167f4efa64ed549bdcfdce9085522b0533fc82
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-11-23 21:34:43 -0600

    add implementation files

--- CurrentAccount.cpp
+++ CurrentAccount.cpp
@@ -1,1 +1,27 @@
 #include "CurrentAccount.h"
+#include <iostream>
+
+CurrentAccount::CurrentAccount(std::string accNum, std::string accHolder, double bal, double limit)
+	: Account(accNum, accHolder, bal), overdraftLimit(limit) {}
+
+void CurrentAccount::displayDetails() {
+	Account::displayDetails();
+	std::cout << "  Overdraft Limit: $" << this->overdraftLimit << std::endl;
+}
+
+void CurrentAccount::withdraw(double amount) {
+	// Allow overdrafts up to the specified limit
+	if (amount <= balance + overdraftLimit) {
+		balance -= amount;
+	}
+	else {
+		cout << "Withdrawal exceeds overdraft limit.\n";
+	}
+}
+
+// Operator to transfer funds from one account to another
+CurrentAccount& CurrentAccount::operator+(Account& other) {
+	this->deposit(other.balance);
+	other.withdraw(other.balance);
+	return *this;
+}
--- SavingsAccount.cpp
+++ SavingsAccount.cpp
@@ -1,1 +1,26 @@
 #include "SavingsAccount.h"
+#include <iostream>
+
+SavingsAccount::SavingsAccount(std::string accNum, std::string accHolder, double bal, double rate)
+	: Account(accNum, accHolder, bal), interestRate(rate) {}
+
+void SavingsAccount::displayDetails() {
+	Account::displayDetails();
+	std::cout << "  Interest Rate: " << this->interestRate*100 << "%" << std::endl;
+}
+
+void SavingsAccount::withdraw(double amount) {
+	// Enforce a minimum balance
+	if (amount <= balance) {
+		balance -= amount;
+	}
+	else {
+		cout << "Insufficient balance.\n";
+	}
+}
+
+SavingsAccount& SavingsAccount::operator+(Account& other) {
+	this->deposit(other.balance);
+	other.withdraw(other.balance);
+	return *this;
+}
